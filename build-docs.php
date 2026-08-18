0388e1a01eeb3f1b4244bf192d21b257dbc90830
Punyapal Shah <mrpunyapal@gmail.com>
2026-08-19 02:41:03 +0530

    docs: add repository and edit page links

--- build-docs.php
+++ build-docs.php
@@ -12,4 +12,6 @@ Docsmith::make()
     ->output(__DIR__ . '/docs')
     ->title('Laravel Undocumented Features')
     ->description('A curated list of Laravel features that are not (or barely) documented in the official Laravel docs.')
+    ->repositoryUrl('https://github.com/mrpunyapal/laravel-undocumented')
+    ->editBranch('main')
     ->build();
